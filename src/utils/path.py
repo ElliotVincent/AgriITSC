482d2a1cbf4d1d9f29cb9803579e709be21ebe3a
Elliot Vincent <44836960+ElliotVincent@users.noreply.github.com>
2023-02-25 16:51:28 +0100

    Delete src directory

--- src/utils/path.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-
-# Project and source files
-PROJECT_PATH = Path(__file__).parent.parent.parent
-CONFIGS_PATH = PROJECT_PATH / 'configs'
-DATASETS_PATH = PROJECT_PATH / 'datasets'
-RESULTS_PATH = PROJECT_PATH / 'results'
